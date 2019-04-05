3d458c00634712350b79a9e1154b50acaee389af
transfer76 <shina76@ukr.net>
2019-04-06 02:02:01 +0300

    Add test for Game_Controller

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -138,6 +138,20 @@ RSpec.describe GamesController, type: :controller do
       expect(flash[:alert]).to be
     end
 
+    it 'uses fifty_fifty help' do
+      expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+      expect(game_w_questions.fifty_fifty_used).to be_falsey
+
+      put :help, id: game_w_questions.id, help_type: :fifty_fifty
+      game = assigns(:game)
+
+      expect(game.finished?).to be_falsey
+      expect(game.fifty_fifty_used).to be_truthy
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to be
+      expect(game.current_game_question.help_hash[:fifty_fifty].count).to eq(2)
+      expect(response).to redirect_to(game_path(game))
+    end
+
     it 'uses audience help' do
       expect(game_w_questions.current_game_question.help_hash[:audience_help]).not_to be
       expect(game_w_questions.audience_help_used).to be_falsey
