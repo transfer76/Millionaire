f2127cc82e95e88adeaf13b9b602c013ed943d85
transfer76 <shina76@ukr.net>
2019-04-06 01:50:22 +0300

    Add tests for model Game_Question

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -18,6 +18,19 @@ RSpec.describe GameQuestion, type: :model do
       expect(game_question.answer_correct?('b')).to be_truthy
     end
 
+    it 'correct .help_hash' do
+      expect(game_question.help_hash).to eq({})
+
+      game_question.help_hash[:some_key1] = 'blabla1'
+      game_question.help_hash['some_key2'] = 'blabla2'
+
+      expect(game_question.save).to be_truthy
+
+      gq = GameQuestion.find(game_question.id)
+
+      expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+    end
+
     it 'correct .level & .text delegates' do
       expect(game_question.text).to eq(game_question.question.text)
       expect(game_question.level).to eq(game_question.question.level)
@@ -29,6 +42,28 @@ RSpec.describe GameQuestion, type: :model do
   end
  
   context 'user helpers' do
+    it 'correct fifty_fifty' do
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+      game_question.add_fifty_fifty
+
+      expect(game_question.help_hash).to include(:fifty_fifty)
+
+      ff = game_question.help_hash[:fifty_fifty]
+      expect(ff).to include('b')
+      expect(ff.size).to eq 2
+    end
+
+    it 'correct friend call' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+
+      game_question.add_friend_call
+      expect(game_question.help_hash).to include(:friend_call)
+      expect(game_question.help_hash[:friend_call]).to be
+
+      fc = game_question.help_hash[:friend_call]
+      expect(fc).to include('считает, что это вариант')
+    end
+
     it 'correct audience_help' do
       expect(game_question.help_hash).not_to include(:audience_help)
 
