9529b15b005d8e45aae56041ab18b2764a4a2d01
transfer76 <shina76@ukr.net>
2019-04-06 18:41:39 +0300

    Add tests for users show

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -28,4 +28,39 @@ RSpec.describe 'users/show', type: :view do
     end
   end
 
+  context 'user alien page' do
+    before(:each) do
+      assign(:user, FactoryGirl.build_stubbed(:user, name: 'Good Programmer'))
+      assign(:games, [FactoryGirl.build_stubbed(:game, id:1,created_at: Time.now, current_level: 2)])
+
+      render
+    end
+
+    it 'render player name' do
+      expect(rendered).to match('Good Programmer')
+    end
+
+    it 'render questions' do
+      expect(rendered).to match('Вопросы')
+    end
+
+    it 'render date' do
+      expect(rendered).to match('Дата')
+    end
+
+    it 'render prize' do
+      expect(rendered).to match('Выигрыш')
+    end
+
+    it 'render link change password' do
+      expect(rendered).not_to match("Сменить имя и пароль")
+    end
+
+    it 'render game show page' do
+      stub_template 'users/show.html.erb' => 'User game goes here'
+      render
+
+      expect(rendered).to have_content 'User game goes here'
+    end
+  end
 end
